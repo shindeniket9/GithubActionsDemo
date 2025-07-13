88273626bd0c3748632e4221cf29f158501801be
shindeniket9 <shindeniket9@gmail.com>
2025-07-13 16:00:34 +0530

    Headless chrome created

--- src/test/java/Demo/Demo.java
+++ src/test/java/Demo/Demo.java
@@ -3,6 +3,7 @@ package Demo;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,15 +13,24 @@ public class Demo {
 
     static {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+        // Set headless if running in CI
+        if (System.getenv("CI") != null) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+        }
+
+        driver = new ChromeDriver(options);
     }
 
     @Test
     public void demo(){
         String url = "https://google.com";
-
         driver.get(url);
-        Assert.assertEquals(driver.getTitle(),"Gooogle");
 
+        System.out.println("Page title: " + driver.getTitle());
+        Assert.assertEquals(driver.getTitle(), "Gooogle"); // Check typo
     }
 }
