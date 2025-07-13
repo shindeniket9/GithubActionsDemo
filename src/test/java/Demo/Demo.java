be84f5f7a129017ee947377a2d2d45b337037408
shindeniket9 <shindeniket9@gmail.com>
2025-07-13 15:54:02 +0530

    Open google in Chrome

--- src/test/java/Demo/Demo.java
+++ src/test/java/Demo/Demo.java
@@ -1,11 +1,26 @@
 package Demo;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Demo {
 
+    public static WebDriver driver;
+
+    static {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
     @Test
     public void demo(){
-        System.out.println("Hello World");
+        String url = "https://google.com";
+
+        driver.get(url);
+        Assert.assertEquals(driver.getTitle(),"Gooogle");
+
     }
 }
